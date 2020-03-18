8e4700b3f4a0a7422017b3c62658b3581a737011
ChesGuev99 <47568678+ChesGuev99@users.noreply.github.com>
2020-03-17 22:21:27 -0600

    .const a=1 . text label diccionarios

--- main.py
+++ main.py
@@ -1,33 +1,48 @@
+constant = {}
+punto_Program = ''
+comandos = []
 
 
 def programa(txt):
-
+    global punto_Program, comandos
     prog_op = open(txt, 'r', encoding='utf-8')
     prog = prog_op.readlines()
-    
-    punto_Program = []
-    constantes = []
-    comandos =[]
-    
-    estado = -1
+
+    estado = -1                     # Empieza en -1 para revisar que no hayan errores
 
     for i in prog:
         linea = espComent(i)
         if linea != '':
             if ".program" in linea:
-                punto_Program = [linea[0:7],linea[9:]]
+                linea.find(".program")
+                punto_Program = linea[9:]       # toma el nombre del programa luego del .program (8 letras) y cambia el estado a 0
                 estado = 0
-            elif ((estado == 1) or (".const" in linea)) and (".text" not in linea):
+            elif ((estado == 1) or (".const" in linea)) and (".text" not in linea): # Si existen constantes, enctra al if y cambia de estado a 1
                 if estado ==-1:
                     print("ERROR: archivo no contiene '.program' ")
                     return 0
-                constantes += [linea]
+                if estado == 1:
+                    constantDicc(linea)          # Envia a linea para agregar al diccionario, en caso de que la linea
+                if len(linea)>len(".const"):     # contenga un .const seguido de la constante, verifica y envia la linea
+                    linea.replace(".const", "")   # solo en caso de que luego de remplazar el espaciado, la linea tenga
+                    linea = linea.replace("➝", "")  # un tama;o mayor a 3 (a=1)
+                    linea = linea.replace("🖵", "")
+                    if len(linea) >= 3:
+                        constantDicc(linea)
+                    elif (len(linea) < 3) and (len(linea) > 0):
+                        print("ERROR: Formato incorrecto en las constantes")
                 estado = 1
             elif estado == 2 or ".text" in linea:
                 if estado == -1:
                     print("ERROR: archivo no contiene '.program' ")
                     return 0
-                comandos += [linea]
+                if estado == 2:
+                    comandos += [linea]          # arega los comandos a la lista luego de encontrar el .text, se utiliza
+                linea = linea.replace(".text", "")  # aux para verificar que no hayan instrucciones en la misma linea del .text
+                aux = linea.replace("➝", "")
+                aux = aux.replace("🖵", "")
+                if len(aux) >= 1:
+                    comandos += [linea]
                 estado = 2
             else:
                 print("ERROR: declaracion de constantes sin un .const")
@@ -35,10 +50,9 @@ def programa(txt):
     if len(comandos) == 0:
         print("ERROR, archivo no contiene '.text' ")
         return 0
-    print(punto_Program, "\n", constantes, "\n", comandos)
 
 
-def espComent(linea):
+def espComent(linea):               # Se utiliza para cambiar todos los espacios y tabs por 🖵 y ➝
     linea = linea.replace("    ", "➝")
     linea = linea.replace(" ", "🖵")
     a = b = 0
@@ -50,13 +64,22 @@ def espComent(linea):
     return linea
 
 
+def constantDicc(linea):            # Elimina los espacios y tabs y toma usando el = el nombre de la variable y el valor
+    global constant                 # se maneja en string por que el valor puede ser de formato 0x000
+    linea = linea.replace("➝", "")
+    linea = linea.replace("🖵", "")
+    igual = linea.find("=")
+    if ("=" in linea) and (igual > 0) and (igual < len(linea)):
+        constant.setdefault(linea[igual-1], linea[igual+1:])
+    else:
+        print("ERROR: Formato de constante incorrecto")
+
+
 def main():
     programa("Programa_Ejemplo.txt")
+    print(punto_Program, "\n", constant, "\n", comandos)
 
 
 main()
-
-
-
 
 
